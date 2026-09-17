70b36aed6b112ad1d4dfaea1d3c527c437aeebbd
devjuh98 <juliagalindo1212@gmail.com>
2026-09-17 00:04:24 -0300

    Adicionando a função para ler o arquivo

--- utils.py
+++ utils.py
@@ -1,38 +1,71 @@
 from itertools import permutations
 
 def ler_mundo(arquivo: str) -> list:
-	"""
-	Lê um arquivo contendo as coordenadas dos pontos de entrega de comida e do ponto de retorno do drone.
+    """
+    Lê um arquivo contendo as coordenadas dos pontos de entrega de comida e do ponto de retorno do drone.
 
-	O conteúdo do arquivo segue este formato matricial:
+    O conteúdo do arquivo segue este formato matricial:
 
-	i j
-	a11 a12 ... a1j
-	a21 a22 ... a2j
-	... ... ... ...
-	ai1 ai2 ... aij
+    i j
+    a11 a12 ... a1j
+    a21 a22 ... a2j
+    ... ... ... ...
+    ai1 ai2 ... aij
 
-	i e j são, respectivamente, os números de linhas e colunas da matriz;
-	axy, em que 1 <= x <= i e 1 <= y <= j, deve assumir um dos seguintes valores:
-		- 0, que é um ponto padrão
-		- R, que é o ponto de retorno do drone
-		- A, B, C, D, E, F... (menos o R), que são os pontos de entrega
+    i e j são, respectivamente, os números de linhas e colunas da matriz;
+    axy, em que 1 <= x <= i e 1 <= y <= j, deve assumir um dos seguintes valores:
+        - 0, que é um ponto padrão
+        - R, que é o ponto de retorno do drone
+        - A, B, C, D, E, F... (menos o R), que são os pontos de entrega
 
-	Parâmetros:
-		arquivo: caminho do arquivo no computador.
+    Parâmetros:
+        arquivo: caminho do arquivo no computador.
 
-	Retorno:
-		Uma lista de tuplas que representam pontos e que seguem o formato (nome, x, y), 
-		em que nome indica o rótulo do ponto, seja ele de retorno (R) ou de entrega (A, B, C, D, E, F... menos o R), 
-		x é o valor inteiro da coordenada horizontal do ponto e y o da coordenada vertical.
+    Retorno:
+        Uma lista de tuplas que representam pontos e que seguem o formato (nome, x, y), 
+        em que nome indica o rótulo do ponto, seja ele de retorno (R) ou de entrega (A, B, C, D, E, F... menos o R), 
+        x é o valor inteiro da coordenada horizontal do ponto e y o da coordenada vertical.
 
-		E, em caso de erro na leitura do arquivo, uma lista vazia.
-	"""
+        E, em caso de erro na leitura do arquivo, uma lista vazia.
+    """
+    pontos = []
 
-	pass
+    try:
+        with open(arquivo, "r") as f:
+            linhas = f.readlines()
+            print(linhas)
+
+            if len(linhas) > 0:
+                cabecalho = linhas[0]
+                partes = cabecalho.split()
+                i = int(partes[0])
+                j = int(partes[1])
+
+                for linha_i in range(1, i+1):
+                    linha_matriz = linhas[linha_i]
+                    separador = linha_matriz.split()
+
+                    for separador_i, valor in enumerate(separador):
+                        x = linha_i-1
+                        y = separador_i
+
+                        if valor != "0":
+                            ponto = (valor, x, y)
+                            pontos.append(ponto)
+                            
+                return pontos
+
+            else:
+                return []
+
+    except Exception:
+        return []
+                         
+                        
+    pass
 
 def obter_percurso(mundo: list) -> str:
-	"""
+    """
     Obtém o menor percurso que o drone deve realizar para entregar as comidas.
 
     Parâmetros:
@@ -45,7 +78,7 @@ def obter_percurso(mundo: list) -> str:
 
         E, em caso de erro na execução da função, uma string vazia.
     """
-	
+    
     try:
         origem = None
         entregas = []
@@ -93,3 +126,6 @@ def obter_percurso(mundo: list) -> str:
 
     except Exception:
         return ""
+
+resultado = ler_mundo("mundo.txt")
+print(resultado)
